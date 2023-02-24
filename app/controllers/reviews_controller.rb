387b2f3030142acaa3d9c9b7e7e7de95a2aaa9db
Joy Danenberg <joy.danenberg@gmail.com>
2023-02-24 16:17:55 +0100

    exo

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,22 @@
 class ReviewsController < ApplicationController
-  def index
+  def new
+    @review = Review.new
   end
 
-  def new
+  def create
+    @review = Review.new(review_params)
+    @review.list = @List
+    @review.save
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  get 'reviews/index'
-  get 'reviews/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-
-  resources :lists, only: [:index, :new, :create, :show, :destroy] do
+  root to: "lists#index"
+  resources :lists, only: [:new, :create, :show, :destroy] do
     resources :bookmarks, only: [:new, :create]
     resources :reviews, only: [:new, :create]
   end
