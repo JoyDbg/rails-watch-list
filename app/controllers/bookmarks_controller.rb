5ffadffe17debcb45365073cc1ce544fa74d1502
Joy Danenberg <joy.danenberg@gmail.com>
2023-02-24 01:06:56 +0100

    exo2

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,30 +1,36 @@
 class BookmarksController < ApplicationController
-  def new
-    @list = List.find(params[:list_id])
+  before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: [:new, :create]
 
+  def new
     @bookmark = Bookmark.new
   end
 
   def create
-    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     if @bookmark.save
       redirect_to list_path(@list)
     else
-      render 'lists/show', status: :unprocessable_entity
+      render :new
     end
   end
 
   def destroy
-    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to list_path(@list), status: :see_other
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
